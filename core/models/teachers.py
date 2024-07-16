13066cb7709076e322c18ead334dd05036ec2064
Vaibhav Kesharwani <vaibhav.vk2128@gmail.com>
2024-07-16 13:40:02 +0530

    Added all the missing endpoints

--- core/models/teachers.py
+++ core/models/teachers.py
@@ -4,10 +4,17 @@ from core.libs import helpers
 
 class Teacher(db.Model):
     __tablename__ = 'teachers'
-    id = db.Column(db.Integer, db.Sequence('teachers_id_seq'), primary_key=True)
+    id = db.Column(db.Integer, db.Sequence(
+        'teachers_id_seq'), primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    created_at = db.Column(db.TIMESTAMP(timezone=True), default=helpers.get_utc_now, nullable=False)
-    updated_at = db.Column(db.TIMESTAMP(timezone=True), default=helpers.get_utc_now, nullable=False, onupdate=helpers.get_utc_now)
+    created_at = db.Column(db.TIMESTAMP(timezone=True),
+                           default=helpers.get_utc_now, nullable=False)
+    updated_at = db.Column(db.TIMESTAMP(
+        timezone=True), default=helpers.get_utc_now, nullable=False, onupdate=helpers.get_utc_now)
 
     def __repr__(self):
         return '<Teacher %r>' % self.id
+
+    @classmethod
+    def get_all(cls):
+        return cls.query.all()
